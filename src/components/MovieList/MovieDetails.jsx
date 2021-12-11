af5d44124f060774923fa9e9c5f7597e24e45208
Victor Lees <vlees14.pa@gmail.com>
2021-12-11 13:06:44 -0500

    creating moviedetails page, not done yet

--- src/components/MovieList/MovieDetails.jsx
+++ src/components/MovieList/MovieDetails.jsx
@@ -2,11 +2,29 @@ import { useHistory, useParams } from 'react-router-dom';
 import { useSelector, useDispatch  } from 'react-redux';
 import { useEffect } from 'react'; 
 
-function (MovieDetail){
+function MovieDetail() {
 
-    const history = useHistory;
-    const dispatch = useDispatch;
+    const history = useHistory();
+    const dispatch = useDispatch();
 
-    const movieItemDetails = useSelector(store => store.selectedMovie)
+    const movieItemDetails = useSelector(store => store.selectedMovie);
+    const genreArray = useSelector(store => store.selectGenreReducer);
 
+    const prevPage= () => {
+        history.push('/');
+    }
+    const { id } useParams();
+
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_MOVIE_DETAILS', payload: {id: id}})
+    })
+
+
+    return(
+        <div className='movieDetails'>
+            <h1>Details for {movieItemDetails}</h1>
+            <img 
+        </div>
+    )
 }
